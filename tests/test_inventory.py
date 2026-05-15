52ff810ea0e58aed21fa1b4aa309cc717255cf98
shohina <shkhamraeva2000@gmail.com>
2026-05-15 15:17:49 +0500

    Tasks 005-010 done

--- tests/test_inventory.py
+++ tests/test_inventory.py
@@ -84,8 +84,10 @@ class TestInventory:
         login_page = LoginPage(page)
         login_page.open()
         login_page.login_procedure(USER1_NAME, USERS_PASSWORD)
+
         inventory_page = InventoryPage(page)
         inventory_page.expect_to_have_url(INVENTORY_URL)
+
         images = inventory_page.get_products_images()
 
         for i, img in enumerate(images):
@@ -96,3 +98,145 @@ class TestInventory:
                 "el => el.complete && el.naturalWidth > 0")
             assert is_loaded, (
                 f"Изображение товара '{src}' не загрузилось или битое")
+
+    @allure.title("Сортировка по цене: низкая → высокая")
+    def test_inv_005(self, page):
+        """
+        005 Выбрать сортировку, проверить порядок цен.
+        Цены идут по возрастанию
+        :param page: фикстура браузера со страницей
+        :return: None
+        """
+        login_page = LoginPage(page)
+        login_page.open()
+        login_page.login_procedure(USER1_NAME, USERS_PASSWORD)
+
+        inventory_page = InventoryPage(page)
+        inventory_page.expect_to_have_url(INVENTORY_URL)
+
+        inventory_page.select_sort("lohi")
+        actual_prices = inventory_page.get_products_prices_as_float()
+        expected_prices = sorted(actual_prices)
+
+        assert actual_prices == expected_prices, \
+            (f"Цены не отсортированы по возрастанию.\n"
+             f"Ожидалось: {expected_prices}\n"
+             f"Получено:  {actual_prices}")
+
+    @allure.title("Сортировка по цене: высокая → низкая")
+    def test_inv_006(self, page):
+        """
+        006 Аналогично, но по убыванию. Цены по убыванию
+        :param page: фикстура браузера со страницей
+        :return: None
+        """
+        login_page = LoginPage(page)
+        login_page.open()
+        login_page.login_procedure(USER1_NAME, USERS_PASSWORD)
+
+        inventory_page = InventoryPage(page)
+        inventory_page.expect_to_have_url(INVENTORY_URL)
+
+        inventory_page.select_sort("hilo")
+        actual_prices = inventory_page.get_products_prices_as_float()
+        expected_prices = sorted(actual_prices, reverse=True)
+
+        assert actual_prices == expected_prices, \
+            (f"Цены не отсортированы по убыванию.\n"
+             f"Ожидалось: {expected_prices}\n"
+             f"Получено:  {actual_prices}")
+
+    @allure.title("Сортировка по названию: A → Z")
+    def test_inv_007(self, page):
+        """
+         007 Проверить алфавитный порядок.
+         Названия отсортированы лексикографически
+        :param page: фикстура браузера со страницей
+        :return: None
+        """
+        login_page = LoginPage(page)
+        login_page.open()
+        login_page.login_procedure(USER1_NAME, USERS_PASSWORD)
+
+        inventory_page = InventoryPage(page)
+        inventory_page.expect_to_have_url(INVENTORY_URL)
+
+        inventory_page.select_sort("az")
+        actual_names = inventory_page.get_products_names()
+        expected_names = sorted(actual_names)
+
+        assert actual_names == expected_names, \
+            (f"Названия не отсортированы по алфавиту A→Z.\n"
+             f"Ожидалось: {expected_names}\n"
+             f"Получено:  {actual_names}")
+
+    @allure.title("Фильтрация после добавления в корзину")
+    def test_inv_008(self, page):
+        """
+        008 Добавить товар → изменить сортировку → товар на месте.
+        Товар не "теряется" при смене сортировки
+        :param page: фикстура браузера со страницей
+        :return: None
+        """
+        login_page = LoginPage(page)
+        login_page.open()
+        login_page.login_procedure(USER1_NAME, USERS_PASSWORD)
+
+        inventory_page = InventoryPage(page)
+        inventory_page.expect_to_have_url(INVENTORY_URL)
+
+        inventory_page.click_btn_add_to_cart()
+        inventory_page.select_sort("lohi")
+        actual_names = inventory_page.get_products_names()
+        assert ExpectedProduct.BACKPACK.title in actual_names, \
+            (
+                f"Товар '{ExpectedProduct.BACKPACK.title}' "
+                f"не найден после смены сортировки.\n"
+                f"Товары на странице: {actual_names}"
+            )
+
+    @allure.title("Клик по изображению товара (если есть переход)")
+    def test_inv_009(self, page):
+        """
+        009 Клик на картинку → переход на детальную (если реализовано).
+        Проверка навигации
+        :param page: фикстура браузера со страницей
+        :return: None
+        """
+        login_page = LoginPage(page)
+        login_page.open()
+        login_page.login_procedure(USER1_NAME, USERS_PASSWORD)
+
+        inventory_page = InventoryPage(page)
+        inventory_page.expect_to_have_url(INVENTORY_URL)
+
+        inventory_page.click_backpack_img()
+
+        actual_url = page.url
+        assert "inventory-item" in actual_url, \
+        (f"Ожидался переход на детальную страницу товара,\n"
+         f"но текущий URL: {actual_url}")
+
+    @allure.title("Проверка кнопки 'Remove' после добавления в корзину")
+    def test_inv_010(self, page):
+        """
+        010 Добавить → кнопка сменилась на "Remove" → нажать →
+        товар исчез из корзины. Бейдж корзины = 0
+        :param page: фикстура браузера со страницей
+        :return: None
+        """
+        login_page = LoginPage(page)
+        login_page.open()
+        login_page.login_procedure(USER1_NAME, USERS_PASSWORD)
+
+        inventory_page = InventoryPage(page)
+        inventory_page.expect_to_have_url(INVENTORY_URL)
+
+        inventory_page.click_btn_add_to_cart()
+        inventory_page.check_btn_remove_visible()
+        inventory_page.click_btn_remove()
+        actual_count = inventory_page.get_cart_badge_count()
+
+        assert actual_count == 0, \
+            (f"Ожидался бейдж корзины = 0, "
+             f"но получено: {actual_count}")
